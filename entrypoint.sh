27b88db87d7eb26979db49d2946574205fa00f22
Ankit Gupta <ankit@reverielabs.com>
2020-06-10 19:53:17 -0400

    another way

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 #echo "$1"
 if  [[ $1 == refs/tags/* ]]
 then
-    echo ::set-output name=tag::${1/refs\/tags\//}
+    echo "::set-output name=tag::${1/refs\/tags\//}"
 elif  [[ $1 == refs/heads/* ]]
 then
-    echo ::set-output name=tag::${1/refs\/heads\//}
+    echo "::set-output name=tag::${1/refs\/heads\//}"
 elif  [[ $1 == refs/pull/* ]]
 then
-    echo ::set-output name=tag::pull-${1/refs\/pull\//}
+    echo "::set-output name=tag::pull-${1/refs\/pull\//}"
 else
-    echo ::set-output name=tag::$1
+    echo "::set-output name=tag::$1"
 fi
 
 
