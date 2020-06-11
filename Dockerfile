6f114c69a29f46b54d32a0e11a0777be14234759
Ankit Gupta <ankit@reverielabs.com>
2020-06-10 20:04:35 -0400

    use bash

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 # Container image that runs your code
 FROM alpine:3.10
+RUN apk add --no-cache bash
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,20 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 echo "$1"
 if  [[ $1 == refs/tags/* ]]
 then
+    echo "tag"
     echo "::set-output name=tag::${1/refs\/tags\//}"
 elif  [[ $1 == refs/heads/* ]]
 then
+    echo "head"
     echo "::set-output name=tag::${1/refs\/heads\//}"
 elif  [[ $1 == refs/pull/* ]]
 then
-    echo "::set-output name=tag::pull"
-    #echo "::set-output name=tag::pull-${1/refs\/pull\//}"
+    echo "pull"
+    echo "::set-output name=tag::pull-${1/refs\/pull\//}"
+else
+    echo "::set-output name=tag::$1"
 fi
-#else
-#    #echo "::set-output name=tag::$1"
-#fi
 
 
