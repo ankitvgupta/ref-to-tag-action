fdb473264bfa2ebbfe24629960d92a945bdca46d
Ankit Gupta <ankit@reverielabs.com>
2020-06-10 20:21:41 -0400

    trying this out

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,21 +11,23 @@ result="$(clean $1)"
 #echo $result
 if  [[ $1 == refs/tags/* ]]
 then
-    echo "tag"
+    # These look something like refs/tags/<tag-name>. Extract <tag-name>.
     result="$(clean ${1/refs\/tags\//})"
     echo "::set-output name=tag::$result"
 elif  [[ $1 == refs/heads/* ]]
 then
-    echo "head"
+	# These look something like refs/heads/<branch-name>. Extract <branch-name>.
     result="$(clean ${1/refs\/heads\//})"
     echo "::set-output name=tag::$result"
 elif  [[ $1 == refs/pull/* ]]
 then
-    echo "pull"
-    result="$(clean ${1/refs\/pull\//})"
-    echo $result
+    # These look something like refs/pull/<pr-number>/merge. Extract <pr-number>
+    remove_front="${1/refs\/pull\//}"
+    remove_back="${remove_front%/merge}"
+    result="$(clean $remove_back)"
     echo "::set-output name=tag::pr-$result"
 else
+	# If it doesn't match one of those, it's just a vanilla ref. Make sure it's docker-compatible.
 	result="$(clean $1)"
     echo "::set-output name=tag::$result"
 fi
