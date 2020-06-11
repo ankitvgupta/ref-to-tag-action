506841979da4bd7d9b7c4189fcd340a97b1e8bbb
Ankit Gupta <ankit@reverielabs.com>
2020-06-10 20:56:32 -0400

    Added example Github Workflow, updated to parse different types. (#1)
    * testing it out
    
    * typo
    
    * execute
    
    * test
    
    * permissions
    
    * one more try
    
    * try bash
    
    * ls
    
    * something simpler
    
    * wrong branch
    
    * try this
    
    * dump context
    
    * new entrypoint
    
    * exctract pull
    
    * another one
    
    * another way
    
    * try
    
    * test
    
    * test'
    
    * echo
    
    * use bash
    
    * try it out in docker
    
    * trying this out
    
    * added a head ref
    
    * oops
    
    * better naming
    
    * which are we using
    
    * bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,42 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-echo "Hello $1"
-echo "::set-output name=tag::$1"
+# Clean the branch name by replacing / with a .
+function clean {
+    local cleaned=`echo $1 | sed 's,/,.,g'`
+    echo "$cleaned"
+}
+
+ref=$1
+head_ref=$2
+
+# If the $head_ref is non-empty (i.e. pull requests), use it. Otherwise, use $ref (pushes).
+if [ -n "$head_ref" ]; then
+    var_to_use=$head_ref
+else
+    var_to_use=$ref
+fi
+
+if  [[ $var_to_use == refs/tags/* ]]
+then
+    # These look something like refs/tags/<tag-name>. Extract <tag-name>.
+    result="$(clean ${var_to_use/refs\/tags\//})"
+    echo "::set-output name=tag::$result"
+elif  [[ $var_to_use == refs/heads/* ]]
+then
+	# These look something like refs/heads/<branch-name>. Extract <branch-name>.
+    result="$(clean ${var_to_use/refs\/heads\//})"
+    echo "::set-output name=tag::$result"
+elif  [[ $var_to_use == refs/pull/* ]]
+then
+    # These look something like refs/pull/<pr-number>/merge. Extract <pr-number>
+    remove_front="${var_to_use/refs\/pull\//}"
+    remove_back="${remove_front%/merge}"
+    result="$(clean $remove_back)"
+    echo "::set-output name=tag::pr-$result"
+else
+	# If it doesn't match one of those, it's just a vanilla ref. Make sure it's docker-compatible.
+	result="$(clean $var_to_use)"
+    echo "::set-output name=tag::$result"
+fi
+
+
